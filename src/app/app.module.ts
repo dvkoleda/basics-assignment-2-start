3a4c3613f6ae4aeda3fab0d404bf5660f398f49f
dvkoleda <dvkoleda@gmail.com>
2017-11-01 18:09:04 +0300

    Assignment is complete.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NameInputComponent } from './name-input/name-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameInputComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/name-input/name-input.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-name-input',
+  templateUrl: './name-input.component.html',
+  styleUrls: ['./name-input.component.css']
+})
+export class NameInputComponent {
+  userName = '';
+
+  constructor() { };
+
+  resetName() {
+    this.userName = '';
+  }
+
+  isUserNameEmpty() {
+    return this.userName === '';
+  }
+}
